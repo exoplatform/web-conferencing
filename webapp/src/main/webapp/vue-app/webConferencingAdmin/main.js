8b7b6547aab7150d5eb37ba6b229d23066120d23
kachankatrin <51235038+kachankatrin@users.noreply.github.com>
2020-09-08 10:52:50 +0300

    change en locales, fix

--- webapp/src/main/webapp/vue-app/webConferencingAdmin/main.js
+++ webapp/src/main/webapp/vue-app/webConferencingAdmin/main.js
@@ -9,7 +9,7 @@ const vuetify = new Vuetify({
 
 // getting language of user
 const lang = (eXo && eXo.env && eXo.env.portal && eXo.env.portal.language) || "en";
-const localePortlet = "locale.portlet";
+const localePortlet = "locale.webconferencing";
 const resourceBundleName = "WebConferencingAdmin";
 const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/${localePortlet}.${resourceBundleName}-${lang}.json`;
 
@@ -19,7 +19,7 @@ export function init(settings) {
     // init Vue app when locale ressources are ready
     new Vue({
       render: h =>
-        h(adminApp, { props: { ...settings, i18n: i18n, language: lang, resourceBundleName: resourceBundleName } }),
+        h(adminApp, { props: { services: {...settings}, i18n: i18n, language: lang, resourceBundleName: resourceBundleName } }),
       i18n,
       vuetify
     }).$mount("#webconferencingAdmin");
