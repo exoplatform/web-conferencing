84e4ea752681d1aeafc96adf5cb0d485aa4f55f4
Peter Nedonosko <pnedonosko@exoplatform.com>
2020-09-09 13:03:24 +0300

    Exclude Vue frontend files from the tagret WAR; cleanup

--- webapp/webpack.dev.js
+++ webapp/webpack.dev.js
@@ -6,7 +6,7 @@ const webpackCommonConfig = require("./webpack.common.js");
 const app = "webconferencing";
 
 // add the server path to your server location path
-const exoServerPath = "../../plf/platform-6.0.0";
+const exoServerPath = "/exo-server";
 
 let config = merge(webpackCommonConfig, {
   output: {
