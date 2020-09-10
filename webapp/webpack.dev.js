ba47410e8d95e18f6a228713209739aa829e089f
Peter Nedonosko <pnedonosko@exoplatform.com>
2020-09-10 17:13:38 +0300

    Add ignored exo-server

--- webapp/webpack.dev.js
+++ webapp/webpack.dev.js
@@ -5,14 +5,14 @@ const webpackCommonConfig = require("./webpack.common.js");
 // the display name of the war
 const app = "webconferencing";
 
-// add the server path to your server location path
-const exoServerPath = "/eXo/jitsi/exo-server";
+// add the server path to your server location path (it's a symlink in webapp folder to a real server')
+const exoServerPath = "./exo-server";
 
 let config = merge(webpackCommonConfig, {
-  output: {
-    path: path.resolve(`${exoServerPath}/webapps/${app}/`)
-  },
-  devtool: "inline-source-map"
+	output: {
+		path: path.resolve(`${exoServerPath}/webapps/${app}/`)
+	},
+	devtool: "inline-source-map"
 });
 
 module.exports = config;
