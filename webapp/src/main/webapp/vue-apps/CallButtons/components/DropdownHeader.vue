1d1bd1e84276bb849da295c8304ed9923e5e062b
Kateryna <kachankatrin@gmail.com>
2020-10-09 17:29:50 +0300

    Fixed rendering providers buttons on page depending on providerType; add style for call-button-containers; fixed hover issue with WebRTC provider

--- webapp/src/main/webapp/vue-apps/CallButtons/components/DropdownHeader.vue
+++ webapp/src/main/webapp/vue-apps/CallButtons/components/DropdownHeader.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="dropdown-header">
-    <i class="uiIconSocPhone uiIconSocBlue"></i>
-    <div id="dropdown-span">{{ placeholder }}</div>
-    <v-divider vertical />
-    <i class="uiIconArrowDown" @click="opendropdowncomponent(); passrefs()"></i>
+  <div class="dropdown-header" @click="opendropdowncomponent(); passrefs()">
+    <div id="dropdown-span">
+      <i class="uiIconSocPhone uiIconSocBlue"></i>
+      <span>{{ placeholder }}</span>
+    </div>
+    <v-divider class="mx-4" vertical />
+    <i class="uiIconArrowDown"></i>
   </div>
 </template>
+
 <script>
 export default {
   props: {
@@ -24,9 +27,30 @@ export default {
   }
 };
 </script>
-<style>
-.dropdown-header {
-  display: inline-flex;
-  align-items: center;
+
+<style scoped lang="less">
+.VuetifyApp {
+  .dropdown-header {
+    display: inline-flex;
+    align-items: center;
+    background-color: white;
+    border: 1px solid rgb(232, 238, 242);
+    border-radius: 3px;
+    width: 100%;
+    #dropdown-span {
+      flex: 3;
+      text-align: center;
+    }
+    i {
+      flex: 1;
+      text-align: center;
+    }
+  }
+  hr {
+    margin: 0;
+  }
+  .v-divider--vertical {
+    min-height: 36px;
+  }
 }
 </style>
