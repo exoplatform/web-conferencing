afdba908999da61f3db74dbb2d3ae4b0279a4983
Nick Riabovol <nick.riabovol@gmail.com>
2020-10-20 18:06:55 +0300

    Add the js promise logic for the create call button method; transfer extensionContainer for mounting

--- webapp/src/main/webapp/vue-apps/CallButtons/main.js
+++ webapp/src/main/webapp/vue-apps/CallButtons/main.js
@@ -1,7 +1,6 @@
 import callButtons from "./components/CallButtons.vue";
 
 Vue.use(Vuetify);
-// const comp = Vue.component("call-button", callButtons);
 const vuetify = new Vuetify({
   dark: true,
   iconfont: "",
@@ -14,41 +13,38 @@ const localePortlet = "locale.webconferencing";
 const resourceBundleName = "WebConferencingClient";
 const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/${localePortlet}.${resourceBundleName}-${lang}.json`;
 
-export function create(context) {
+export function create(context, extensionContainer) {
   const callContext = context;
-  console.log(callContext, "context in main");
-  const vmComp = exoi18n.loadLanguageAsync(lang, url).then((i18n) => {
-    const comp = Vue.component("call-button", {
-      render: function(createElement) {
-        return createElement(
-          callButtons,
-          { props: { callContext, i18n, language: lang, resourceBundleName } } // tag name
-        );
-      },
-    });
-    return new Vue({
-      el: "#call-button-container",
-      // data,
-      comp,
-      render: (h) => {
-        console.log("INIT");
-        return h(comp, 
-          {
-          props: { callContext, i18n, language: lang, resourceBundleName },
+  const result = new Promise((resolve, reject) => {
+    exoi18n.loadLanguageAsync(lang, url).then((i18n) => {
+      const comp = Vue.component("call-button", {
+        render : function (createElement) {
+          return createElement(
+            callButtons,
+            {props : {...callContext, i18n, language : lang, resourceBundleName}} // tag name
+          );
+        },
+      });
+      const vmComp = new Vue({
+        el : extensionContainer,
+        comp,
+        render : (h) => {
+          return h(comp, {
+              props : {...callContext, i18n, language : lang, resourceBundleName},
+            },
+            i18n,
+            vuetify
+          );
+        },
+      });
+
+      resolve({
+        update : function (context) {
+          vmComp._vnode.data.props.callContext = context;
         }
-        )
-      },
+      });
     });
-    // return vm;
   });
-  // return vmComp;
-  return {
-    update: function(context) {
-      console.log("in update");
-      vmComp.then(vm => {
-        console.log(vm._vnode.data.props.callContext, context)
-        return vm._vnode.data.props.callContext = context;
-      })
-    }}
-  // return vmComp;
+
+  return result;
 }
