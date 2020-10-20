13bd917765f4c21030a5b6648ebed49bc431dd3e
Nick Riabovol <nick.riabovol@gmail.com>
2020-10-20 20:36:59 +0300

    Configure the user profile popup extension for call button; setup extension configs; cleanup webconferencing-call-plugin.js

--- webapp/src/main/webapp/js/webconferencing-call-plugin.js
+++ webapp/src/main/webapp/js/webconferencing-call-plugin.js
@@ -1,183 +1,180 @@
+const EVENT_ROOM_SELECTION_CHANGED = "exo-chat-selected-contact-changed";
+
+let userProfilePopupButton;
+
 const webconferencingExts = [
   {
-    target: "chat",
-    type: "room-action-component",
+    target : "chat",
+    type : "room-action-component",
     // configuration defined here is used in exo-addons\web-conferencing\webapp\src\main\webapp\vue-apps
     // \Call\components\CallButtons.vue with
     // exo-addons\chat-application\application\src\main\webapp\vue-app\components\ExoChatRoomDetail.vue and connects them
     // key should be unique and used in parent component as a ref to WebConferencingCall component
-    key: "callButton",
-    rank: 20,
+    key : "chatCallButton",
+    rank : 20,
     // iconName is a name of the icon which is displayed on action button with 'onExecute' action
     // iconName should be one of the names, supported by vuetify 'v-icon' component (https://vuetifyjs.com/en/components/icons/)
     // if it should be custom icon that isn't supported by vuetify iconClass instead of iconName should be used
-    iconName: "callButton",
+    iconName : "callButton",
     // appClass is a class of container which consist of action button and WebConferencingCall component
-    appClass: "webconferencingCallButton",
+    appClass : "webconferencingCallButton",
     // component has property which will be passed to dynamic component inside parent
     // (https://vuejs.org/v2/guide/components.html#Dynamic-Components)
-    component: {
+    component : {
       // name should be the name registered via Vue.component (https://vuejs.org/v2/guide/components-registration.html#Component-Names)
-      name: "call-button",
+      name : "call-button",
       // events are passed to custom DynamicEvents directive (https://vuejs.org/v2/guide/custom-directive.html)
-      events: [],
+      events : [],
     },
     // init call button context in chat
-    init: function (chat) {
+    init : function (extensionContainer, chat) {
       require([
         "SHARED/webConferencing",
         "SHARED/webConferencingCallButton",
       ], function (webConferencing, callButtons) {
-        //webConferencing.initChatContext(chat);
-        // let callBtn;
-        // let cont;
-        // let buttn; 
-        // const getCurrentButton = 
-        let btn;
-        //let cont;
-          webConferencing.createChatContext(chat).then(
-            (context) => {
-              callButtons.create(context).then(button => {
-                document.addEventListener("exo-chat-selected-contact-changed", function(target) {
-                webConferencing.createChatContext(chat, target).then(cntxt => { return button.update(cntxt)})
-              })});
-              //document.addEventListener("exo-chat-selected-contact-changed", function() {
-              //  webConferencing.createChatContext(chat).then(cntxt => {console.log(btn); return btn.update(cntxt)})
-              //})
-              // return btn;
-            }
-          );
-          // webConferencing.createChatContext(chat).then(
-          //   (context) => {
-          //     btn = callButtons.create(context);
-          //     // return btn;
-          //   },
-          //   document.addEventListener("exo-chat-selected-contact-changed", function() {
-          //     webConferencing.createChatContext(chat).then(cntxt => { return btn.update(cntxt)})
-          //   })
-          // );
-        
-        // getCurrentButton()
-        // document.addEventListener(
-        //   "exo-chat-selected-contact-changed",
-        //   async function () {
-
-        //     webConferencing.createChatContext(chat).then((context) => {
-        //       callButtons.create(context).update(context);
-        //     });
-        //     return await getCurrentButton().update(await cont)
-        //     //const updBtn = await buttn;
-        //     //const cont = await localCont
-        //     // const res = updBtn.then(btn => btn.update(localContext))
-        //     //return updBtn.update(cont);
-        //     // return res;
-        //     // return await button.update(webConferencing.createChatContext(chat))
-
-        //     // await button
-
-        //     // webConferencing
-        //     //   .createChatContext(chat)
-        //     //   .then((context) => {
-        //     //     // callBtn = callButtons.create(context);
-        //     //     // return callButtons.create(context).then((button) => {
-        //     //     callButtons.create(context);
-        //     //     //     return button;
-        //     //     //   })
-        //     //     //.then((res) => res);
-        //     //   })
-        //     // .then((res) => {
-        //     //   res.update(webConferencing.createChatContext(chat));
-        //     // });
-        //     //     // await button.update(await webConferencing.createChatContext(chat));
-
-        //     //     //   // TODO remove previous one added button here
-        //     //     //   //callButtons.destroy();
-        //     //     //   // TODO add all buttons from here
-        //     //     //   // callButtons.init(target);
-        //     //     //   button.update(await webConferencing.createChatContext(chat), target);
-        //   }
-        // );
+        webConferencing.createChatContext(chat).then(
+          (context) => {
+            callButtons.create(context, extensionContainer).then(button => {
+              document.addEventListener(EVENT_ROOM_SELECTION_CHANGED, function (target) {
+                webConferencing.createChatContext(chat, target).then(contextFromEvent => {
+                  button.update(contextFromEvent);
+                });
+              });
+            });
+          }
+        );
       });
     },
     // enabled just show that this extension is enabled, if enabled: false WebConferencingCallComponent will not appear on page
-    enabled: true,
+    enabled : true,
   },
   {
-    target: "chat",
-    type: "mini-chat-title-action-component",
+    target : "chat",
+    type : "mini-chat-title-action-component",
     // configuration defined here is used in exo-addons\web-conferencing\webapp\src\main\webapp\vue-apps
     // \Call\components\CallButtons.vue with
     // exo-addons\chat-application\application\src\main\webapp\vue-app\components\modal\ExoChatDrawer.vue and connects them
     // key should be unique and used in parent component as a ref to WebConferencingCall component
-    key: "callButton",
-    rank: 20,
+    key : "miniChatCallButton",
+    rank : 21,
     // iconName is a name of the icon which is displayed on action button with 'onExecute' action
     // iconName should be one of the names, supported by vuetify 'v-icon' component (https://vuetifyjs.com/en/components/icons/)
     // if it should be custom icon that isn't supported by vuetify iconClass instead of iconName should be used
-    iconName: "callButton",
+    iconName : "callButton",
     // appClass is a class of container which consist of action button and WebConferencingCall component
-    appClass: "webсonferencingCallButton", // TODO seems here we should add a MINI-class?
+    appClass : "webсonferencingCallButton", // TODO seems here we should add a MINI-class?
     // component has property which will be passed to dynamic component inside parent
     // (https://vuejs.org/v2/guide/components.html#Dynamic-Components)
-    component: {
+    component : {
       // name should be the name registered via Vue.component (https://vuejs.org/v2/guide/components-registration.html#Component-Names)
-      name: "call-button",
+      name : "call-button",
       // events are passed to custom DynamicEvents directive (https://vuejs.org/v2/guide/custom-directive.html)
-      events: [],
+      events : [],
     },
     // init call button context in mini chat
-    init: function (chat) {
+    init : function (extensionContainer, chat) {
       require([
         "SHARED/webConferencing",
         "SHARED/webConferencingCallButton",
-      ], function (webConferencing, webConferencingCallButton) {
-        //webConferencing.initChatContext(chat); // TODO should we point to a context it's a mini-chat here?
-        var settings = {};
-        webConferencingCallButton.init(settings); // TODO not good as we cheat ourselves by this here!
+      ], function (webConferencing, callButtons) {
+        webConferencing.createChatContext(chat).then(
+          (context) => {
+            callButtons.create(context, extensionContainer).then(button => {
+              document.addEventListener(EVENT_ROOM_SELECTION_CHANGED, function (target) {
+                webConferencing.createChatContext(chat, target).then(contextFromEvent => {
+                  button.update(contextFromEvent);
+                });
+              });
+            });
+          }
+        );
       });
     },
     // enabled just show that this extension is enabled, if enabled: false WebConferencingCallComponent will not appear on page
-    enabled: true,
+    enabled : true,
   },
   {
-    target: "space-menu",
-    type: "action-component",
+    target : "space-menu",
+    type : "action-component",
     // configuration defined here is used in exo-addons\web-conferencing\webapp\src\main\webapp\vue-apps
     // \Call\components\CallButtons.vue with
     // exo-addons\chat-application\application\src\main\webapp\vue-app\components\ExoChatRoomDetail.vue and connects them
     // key should be unique and used in parent component as a ref to WebConferencingCall component
-    key: "callButton",
-    rank: 21,
+    key : "spaceMenuCallButton",
+    rank : 22,
     // iconName is a name of the icon which is displayed on action button with 'onExecute' action
     // iconName should be one of the names, supported by vuetify 'v-icon' component (https://vuetifyjs.com/en/components/icons/)
     // if it should be custom icon that isn't supported by vuetify iconClass instead of iconName should be used
-    iconName: "callButton",
+    iconName : "callButton",
     // appClass is a class of container which consist of action button and WebConferencingCall component
-    appClass: "webсonferencingCallButton",
+    appClass : "webсonferencingCallButton",
     // component has property which will be passed to dynamic component inside parent
     // (https://vuejs.org/v2/guide/components.html#Dynamic-Components)
-    component: {
+    component : {
       // name should be the name registered via Vue.component (https://vuejs.org/v2/guide/components-registration.html#Component-Names)
-      name: "call-button",
+      name : "call-button",
       // events are passed to custom DynamicEvents directive (https://vuejs.org/v2/guide/custom-directive.html)
-      events: [],
+      events : [],
     },
     // init call button context in space
-    init: function (spaceId, userId) {
+    init : function (extensionContainer, spaceId) {
       require([
         "SHARED/webConferencing",
         "SHARED/webConferencingCallButton",
-      ], function (webConferencing, webConferencingCallButton) {
-        webConferencing.createSpaceContext(spaceId, userId);
+      ], function (webConferencing, callButtons) {
+        webConferencing.createSpaceContext(spaceId)(
+          (context) => {
+            callButtons.create(context, extensionContainer).then(button => {
+              //button.update(context);  // don't need
+            });
+          }
+        );
         var settings = {
-          targetExtensionPoint: "space",
+          targetExtensionPoint : "space",
         };
         webConferencingCallButton.init(settings);
       });
     },
     // enabled just show that this extension is enabled, if enabled: false WebConferencingCallComponent will not appear on page
-    enabled: true,
-  } /*
+    enabled : true,
+  },
+  {
+    target : "user-profile-popup",
+    type : "exo-social-user-popup-component",
+    // configuration defined here is used in exo-addons\web-conferencing\webapp\src\main\webapp\vue-apps
+    // \Call\components\CallButtons.vue with
+    // social\webapp\resources\src\main\webapp\javascript\eXo\UIUserProfilePopup.js and connects them
+    // key should be unique and used in parent component as a ref to WebConferencingCall component
+    key : "userProfilePopupCallButton",
+    rank : 23,
+    iconName : "callButton",
+    appClass : "webсonferencingCallButton",
+    component : {
+      name : "call-button",
+      events : [],
+    },
+    // init call button context in user profile popup
+    init : function (extensionContainer, userId) {
+      require([
+        "SHARED/webConferencing",
+        "SHARED/webConferencingCallButton",
+      ], function (webConferencing, callButtons) {
+        if (userProfilePopupButton) {
+          // TO DO delete old button
+          //something like userProfilePopupButton.destroy();
+        }
+
+        webConferencing.createUserContext(userId).then((context) => {
+          callButtons.create(context, extensionContainer).then((button) => {
+            userProfilePopupButton = button;
+            //button.update(context);  // don't need
+          });
+        });
+      });
+    },
+    // enabled just show that this extension is enabled, if enabled: false WebConferencingCallComponent will not appear on page
+    enabled : true,
+  }/*
   // an example of the extension with DOM elements
 ,{
   target : "chat",
@@ -220,10 +217,4 @@ require(["SHARED/extensionRegistry"], function (extensionRegistry) {
       extension
     );
   }
-
-  // TODO it's not right thing to fire such events from not a Chat app!!
-  // dispatch the event about adding an extension in the chat
-  //document.dispatchEvent(new CustomEvent("chat-room-action-components-updated"));
-  // dispatch the event about adding an extension in the mini chat
-  //document.dispatchEvent(new CustomEvent("mini-chat-title-action-components-updated"));
 });
