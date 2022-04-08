7ea4d26002ce78cf5f7110c4a70253f1d416b85f
Romain Dénarié <romain.denarie@exoplatform.com>
2022-04-08 15:06:50 +0200

    Update CallRecordingNotificationListener.java

--- services/src/main/java/org/exoplatform/webconferencing/listeners/CallRecordingNotificationListener.java
+++ services/src/main/java/org/exoplatform/webconferencing/listeners/CallRecordingNotificationListener.java
@@ -1,5 +1,6 @@
-/***************************************************************************
+/*
  * Copyright (C) 2022 eXo Platform SAS.
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -12,7 +13,7 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
- ***************************************************************************/
+ */
 package org.exoplatform.webconferencing.listeners;
 
 import org.exoplatform.commons.api.notification.NotificationContext;
