076009507234ffd3dd118d02297bc134ce46e1ca
AmineFourati <afourati@exoplatform.com>
2022-04-22 10:14:22 +0100

    removed closeWindow method related to the exit screen button

--- webapp/src/main/webapp/js/webconferencing.js
+++ webapp/src/main/webapp/js/webconferencing.js
@@ -2024,6 +2024,5 @@
 	});
 
 	log.trace("< Loaded at " + location.origin + location.pathname);
-	webConferencing.closeWindow = function(){window.close();};
 	return webConferencing;
 })($, cCometD);
