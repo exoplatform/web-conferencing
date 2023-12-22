a0e5431f63ec5550b49fb4549787f1a89b232d7b
Romain Dénarié <romain.denarie@exoplatform.com>
2023-12-22 20:17:36 +0100

    remove log

--- webrtc/webapp/src/main/webapp/vue-app/webrtc-connector/main.js
+++ webrtc/webapp/src/main/webapp/vue-app/webrtc-connector/main.js
@@ -1,5 +1,4 @@
 window.require(['SHARED/webConferencing','SHARED/webConferencing_webrtc'], function(webConferencing,webConferencing_webrtc) {
-  console.log('init webrtc connector',webConferencing_webrtc,webConferencing);
   if (webConferencing_webrtc) {
     fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/webrtc/webconferencing/connectorsettings`, {
       credentials: 'include',
