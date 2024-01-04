e4edff6f96ff24c2c97284fd42bc47ad3451b877
Ali HAMDI <ahamdi@exoplatform.com>
2024-01-04 12:03:10 +0100

    fix: Webconferencing admin portlet not working - EXO-67648  (#210)
    * fix: Webconferencing admin portlet not working - EXO-67648
    
    Before this fix, the admin portlet not working due the fact that WebConferencingPortelt was added in dynamic container
    As this container is not present in administration site, the providers are not load
    This fix change to way providers are loaded to use a processus similar to extensionRegistry, and js are now loaded by the portlet which need it (WebconferencingAdminPortlet)
    
    ---------
    
    Co-authored-by: Romain Dénarié <romain.denarie@exoplatform.com>

---
+++ webrtc/webapp/src/main/webapp/vue-app/webrtc-connector/main.js
@@ -0,0 +1,20 @@
+window.require(['SHARED/webConferencing','SHARED/webConferencing_webrtc'], function(webConferencing,webConferencing_webrtc) {
+  if (webConferencing_webrtc) {
+    fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/webrtc/webconferencing/connectorsettings`, {
+      credentials: 'include',
+      method: 'GET',
+    }).then((resp) => {
+      if (!resp || !resp.ok) {
+        throw new Error('Error while getting webrtc provider configuration');
+      } else {
+        return resp.json();
+      }
+    }).then((data) => {
+      webConferencing_webrtc.configure(data);
+      webConferencing.addProvider(webConferencing_webrtc);
+    });
+  }
+});
+
+
+
