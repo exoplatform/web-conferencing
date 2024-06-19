06da9fed01f4fd81a719f78f58ef8ab1da53d7f8
Ali HAMDI <ahamdi@exoplatform.com>
2024-06-19 14:14:32 +0100

    fix: disable Call button in the user card for logged-in user - EXO-71739 (#314)
    This fix will disable the Call button for the logged in user

--- webapp/src/main/webapp/js/webconferencing-call-plugin.js
+++ webapp/src/main/webapp/js/webconferencing-call-plugin.js
@@ -131,7 +131,7 @@
       // enabled just show that this extension is enabled, if enabled: false WebConferencingCallComponent will not appear on page
       enabled: function(element) {
         //return true only if the extension is used for a user card. We dont want it on a space card for the moment
-        return element?.username;
+        return element?.username && element?.username !== eXo.env.portal.userName;
       }
     },
     {
