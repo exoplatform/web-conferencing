9ce234c38e8b91ad899283c64160c50ef9db5781
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2026-09-08 14:41:28 +0100

    fix: Avoid NPE when reading current calls of a user - EXO-90077 (#403)
    Prior to this change, when reading the list of calls of a user, an NPE is sometimes thrown especially when the user is no more member of a deleted space. This change ensures to not have an NPE.

--- services/src/main/java/org/exoplatform/webconferencing/WebConferencingService.java
+++ services/src/main/java/org/exoplatform/webconferencing/WebConferencingService.java
@@ -41,6 +41,7 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
